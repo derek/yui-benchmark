ced370eee0c8b14248fe0d0b09d10c899e759b2c
Derek Gathright <drgath@gmail.com>
2013-02-13 14:10:14 -0800

    ybench client now returns a BenchmarkJS instance

--- assets/ybench_client.js
+++ assets/ybench_client.js
@@ -6,40 +6,55 @@ YUI.add('ybench', function (Y, NAME) {
         var ua = null;
         Y.each(Y.UA, function(v, k) {
             if (!Y.Lang.isFunction(v) && v && Y.Lang.isNumber(v)) {
-                ua = k + v;
+                ua = k + " " + v;
             }
         });
         return ua;
     }
 
     function YBench(config) {
+        var self = this;
 
-        Bench = new Benchmark(config.title, config.fn, {
-            onComplete: function () {
-                var result = {
-                    component: config.component,
-                    title: config.title,
-                    yuiVersion: YUI.version,
-                    UA: getUA(),
-                    ops: this.hz,
-                    runs: this.stats.sample.length,
-                    deviation: this.deviation,
-                    mean: this.stats.mean,
-                    moe: this.stats.moe,
-                    rme: this.stats.rme,
-                    sem: this.stats.sem,
-                    variance: this.stats.variance
-                };
+        this.component = config.component;
+        this.title = config.title;
+        this.yuiVersion = YUI.version;
+        this.UA = getUA();
 
-                xhr("/write", function (err, data, xhr){
-                    window.location = JSON.parse(data).location;
-                }, 'POST', 'result=' + JSON.stringify(result));
-            }
+        if (config.type === "benchmarkjs") {
+            this.bench = new Benchmark.Suite(this.title);
+
+            this.bench.on('complete', function () {
+                var results = this[0];
+                self.setValue(results.hz, results.stats);
+                console.log(this);
+            });
+        }
+
+        return this.bench;
+    }
+
+    YBench.prototype.setValue = function (val, stats) {
+        var self = this,
+            stats = stats || {};
+            
+        self._sendResult({
+            component: self.component,
+            title: self.title,
+            yuiVersion: self.yuiVersion,
+            UA: self.UA,
+            value: val,
+            stats: stats
         });
     }
 
     YBench.prototype.go = function () {
         Bench.run({async:true});
+    }
+
+    YBench.prototype._sendResult = function (result) {
+        xhr("/write", function (err, data, xhr){
+            window.location = JSON.parse(data).location;
+        }, 'POST', 'result=' + JSON.stringify(result));
     }
 
     Y.Bench = YBench;
