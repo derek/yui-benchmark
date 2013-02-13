b593b554882a808147baec12ffd70d5cad405d20
Derek Gathright <drgath@gmail.com>
2013-02-12 16:17:37 -0800

    Replaced node-phantom with spawn.

---
+++ scripts/load_url.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env phantomjs
+
+var page = require('webpage').create(),
+    system = require('system');
+
+if (system.args.length === 1) {
+    console.log('No input path specified');
+    phantom.exit();
+}
+
+path = system.args[1];
+
+page.open(path, function (status) {
+    if (status !== 'success') {
+        console.log('FAIL to load the address');
+    }
+});
+
+// page.onConsoleMessage = function(msg) {
+//     phantom.exit();
+// };
