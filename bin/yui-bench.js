669eeeb42742a9104f283572fb1919cdbc6164c3
Derek Gathright <drgath@gmail.com>
2013-02-15 16:43:04 -0800

    Added license header

--- bin/yui-bench.js
+++ bin/yui-bench.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/*
+Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://yuilibrary.com/license/
+*/
+
 // Import required modules
 var path = require("path"),
 	nopt = require("nopt"),
