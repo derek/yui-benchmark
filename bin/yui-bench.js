d2ffef6ff917525e78341e40132034199c7ecdb8
Derek Gathright <drgath@gmail.com>
2013-02-19 11:30:11 -0800

    opts->options, also cleanup of options logic

--- bin/yui-bench.js
+++ bin/yui-bench.js
@@ -6,20 +6,4 @@ Code licensed under the BSD License:
 http://yuilibrary.com/license/
 */
 
-// Import required modules
-var path = require("path"),
-    fs = require("fs"),
-    options = require("../lib/opts");
-    
-// Make sure some important files exist
-if (!options.yuipath || !fs.existsSync(path.join(options.yuipath, 'build/yui/yui.js'))) {
-    console.log("yui.js not found. Check --yuipath");
-    return false;
-}
-
-if (!options.source || !fs.existsSync(path.join(options.source))) {
-    console.log("source not found. Check --source");
-    return false;
-}
-
 require('../lib/app.js')();
