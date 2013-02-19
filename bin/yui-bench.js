e2b9b777be21cadb9faac0fd3f5a8cff83441cb7
Derek Gathright <drgath@gmail.com>
2013-02-19 10:30:24 -0800

    Whitespace - tabs to spaces

--- bin/yui-bench.js
+++ bin/yui-bench.js
@@ -8,18 +8,18 @@ http://yuilibrary.com/license/
 
 // Import required modules
 var path = require("path"),
-	fs = require("fs"),
-	options = require("../lib/opts");
-	
+    fs = require("fs"),
+    options = require("../lib/opts");
+    
 // Make sure some important files exist
 if (!options.yuipath || !fs.existsSync(path.join(options.yuipath, 'build/yui/yui.js'))) {
-	console.log("yui.js not found. Check --yuipath");
-	return false;
+    console.log("yui.js not found. Check --yuipath");
+    return false;
 }
 
 if (!options.source || !fs.existsSync(path.join(options.source))) {
-	console.log("source not found. Check --source");
-	return false;
+    console.log("source not found. Check --source");
+    return false;
 }
 
 require('../lib/app.js')();
