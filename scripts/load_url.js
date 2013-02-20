7579f784cd295a6606b8985054889a093eaa69e1
Derek Gathright <drgath@gmail.com>
2013-02-20 03:34:05 -0800

    Corrected logic to allow many result sets per test.  Also, phantomjs works again.

--- scripts/load_url.js
+++ scripts/load_url.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env phantomjs
+
 /*
 Copyright (c) 2013, Yahoo! Inc. All rights reserved.
 Code licensed under the BSD License:
 http://yuilibrary.com/license/
 */
-#!/usr/bin/env phantomjs
 
 var page = require('webpage').create(),
     system = require('system');
