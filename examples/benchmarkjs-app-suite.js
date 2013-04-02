7eca77aff0a112ed093d3d572178e61a00958fa4
Derek Gathright <drgath@gmail.com>
2013-04-02 13:46:24 -0700

    * Adapted source ingestion to take accept current/unmodified yui3 perf tests * Added support for an HTML document as an input source * Renamed executable from `yui-benchmark` to `yb`

--- examples/benchmarkjs-app-suite.js
+++ examples/benchmarkjs-app-suite.js
@@ -1,41 +1,25 @@
 YUI.add('benchmarkjs-app-suite', function (Y, NAME) {
 
-    var suite = Y.Benchmark.suite;
+    var suite = Y.BenchmarkSuite = new Benchmark.Suite;
     
 	// -- Y.Model ------------------------------------------------------------------
-	suite.add({
-		Y: Y,
-		name: 'Y.Model: Instantiate a bare model', 
-		fn: function () {
-		    var model = new Y.Model();
-		}
+	suite.add('Y.Model: Instantiate a bare model', function () {
+	    var model = new Y.Model();
 	});
 
-	suite.add({
-		Y: Y,
-		name: 'Y.Model: Subclass and instantiate a bare model', 
-		fn: function () {
-		    var MyModel = Y.Base.create('myModel', Y.Model, []),
-		        model   = new MyModel();
-	    }
+	suite.add('Y.Model: Subclass and instantiate a bare model', function () {
+	    var MyModel = Y.Base.create('myModel', Y.Model, []),
+	        model   = new MyModel();
 	});
 
 	// -- Y.View -------------------------------------------------------------------
-	suite.add({
-		Y: Y,
-		name: 'Y.View: Instantiate a bare view', 
-		fn: function () {
-		    var view = new Y.View();
-		}
+	suite.add('Y.View: Instantiate a bare view', function () {
+	    var view = new Y.View();
 	});
 
-	suite.add({
-		Y: Y,
-		name: 'Y.View: Instantiate and subclass a bare view',
-		fn: function () {
-		    var MyView = Y.Base.create('myView', Y.View, []),
-		        view   = new MyView();
-		}
+	suite.add('Y.View: Instantiate and subclass a bare view', function () {
+	    var MyView = Y.Base.create('myView', Y.View, []),
+	        view   = new MyView();
 	});
 	
 }, '@VERSION@', {requires: ['app']});
