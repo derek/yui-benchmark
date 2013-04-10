a6596c2601dd36450710d7efe71ee39f08c0c6ec
Derek Gathright <drgath@gmail.com>
2013-04-10 16:50:13 -0700

    Added unit tests and a few fixes

---
+++ tests/test-log.js
@@ -0,0 +1,3 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    log = require('../lib/log');
---
+++ tests/test-site.js
@@ -0,0 +1,3 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    parse = require('../lib/site');
