9af2574f5f077d8c987a8f25d21a3bf5e5a90e47
Derek Gathright <drgath@gmail.com>
2013-05-21 16:20:31 -0700

    Saving point (WIP)

---
+++ bin/parser.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/**
+Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://yuilibrary.com/license/
+*/
+
+require('../lib/parser/parser.js');
