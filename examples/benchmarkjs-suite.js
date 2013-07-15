02111ad50eb6df2358d8de84d651cd80a783c387
Derek Gathright <drgath@gmail.com>
2013-07-15 01:06:56 -0700

    Big cleanup to remove more extraneous code and serve YUI and assets by Task

--- examples/benchmarkjs-suite.js
+++ examples/benchmarkjs-suite.js
@@ -1,39 +1,18 @@
-/**
-Copyright (c) 2013, Yahoo! Inc. All rights reserved.
-Code licensed under the BSD License:
-http://yuilibrary.com/license/
-*/
-YUI.add('benchmarkjs-suite', function (Y, NAME) {
+YUI.add('benchmarkjs-suite', function (Y) {
 
-    var suite = Y.BenchmarkSuite = new Benchmark.Suite,
-        container,
-        scrollview;
-    
-    container = document.createElement('div')
-    container.id = "container";
-    document.body.appendChild(container);
-
-    suite.add('ScrollView: Create', function () {
-        scrollview = new Y.ScrollView({
-            render: container
-        });
-    }, {
-        'setup': function () {
-            var test = 'asd';
-        },
-        'teardown' : function () {
-            var blah = '123';
-        }
-    });
-
-    suite.add({
-        name: 'ScrollView: Create & Destroy', 
+    var suite = Y.BenchmarkSuite = new Benchmark.Suite();
+    var bar = 'bar';
+    suite.add('foo', {
         fn: function () {
-            scrollview = new Y.ScrollView({
-                render: container
-            });
-            scrollview.destroy();
+            // fn
+            console.log(bar, 1);
+            // /fn
+        },
+        setup: function () {
+            // setup
+            console.log(bar, 2);
+            // /setup
         }
     });
 
-}, '@VERSION@', {requires: ['scrollview-base']});
+}, '@VERSION@', {requires: ['datatable']});
