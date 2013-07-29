34062ac067ca7677f4e8ff0f7b86d7e7f3a5a971
Derek Gathright <drgath@gmail.com>
2013-07-29 13:32:44 -0700

    Tweaked linting scripts

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -1,9 +1,11 @@
 # #!/bin/sh
-# SRC_DIR=${SRC_DIR:-.}
-# LINT_OUTPUT_DIR=${LINT_OUTPUT_DIR:-tmp}
-# cd ${SRC_DIR}
+SRC_DIR=${SRC_DIR:-.}
+LINT_OUTPUT_DIR=${LINT_OUTPUT_DIR:-tmp}
 
-jshint --config ./node_modules/yui-lint/jshint.json \
+cd ${SRC_DIR}
+
+./node_modules/.bin/jshint \
+ 	--config ./node_modules/yui-lint/jshint.json \
 	./lib/app/*.js \
 	./bin/*.js \
-	./lib/assets/*.js
+	./lib/assets/*.js > ${LINT_OUTPUT_DIR}/jslint.html
