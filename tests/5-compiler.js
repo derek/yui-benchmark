072b34af60286f79813c264eeb2db7365bae1113
Derek Gathright <drgath@gmail.com>
2013-07-29 11:24:02 -0700

    Added additional unit testing

--- tests/5-compiler.js
+++ tests/5-compiler.js
@@ -14,9 +14,13 @@ var vows = require('vows'),
     vm = require('vm'),
     crypto = require('crypto'),
     compile = require('../lib/app/compiler'),
-    srcConfig = path.join(__dirname, 'assets/5/in/config-1.js'),
+    srcConfig1 = fs.readFileSync(path.join(__dirname, 'assets/5/in/config-1.js')),
+    srcConfig2 = fs.readFileSync(path.join(__dirname, 'assets/5/in/config-2.js')),
+    srcConfig3 = fs.readFileSync(path.join(__dirname, 'assets/5/in/config-3.js')),
+    perfSuiteSrc = fs.readFileSync(path.join(__dirname, '../lib/assets/perf-suite.js')),
     context = {suite: null},
-    suite;
+    assetPath = path.join(__dirname, './assets/5/in/'),
+    suite1, suite2, suite3;
 
     // Useful to hash long strings (HTML docs) because if they fail, Vows' diff of expected/actual takes forevs to generate
     function md5(str) {
@@ -24,27 +28,35 @@ var vows = require('vows'),
     }
 
     // Turn the config string into an object
-    vm.runInNewContext(fs.readFileSync(path.join(__dirname, '../lib/assets/perf-suite.js')) + fs.readFileSync(srcConfig), context);
-    suite = context.suite.exportConfig();
+    vm.runInNewContext(perfSuiteSrc + srcConfig1, context);
+    suite1 = context.suite.exportConfig();
+
+    // Turn the config string into an object
+    vm.runInNewContext(perfSuiteSrc + srcConfig2, context);
+    suite2 = context.suite.exportConfig();
+
+    // Turn the config string into an object
+    vm.runInNewContext(perfSuiteSrc + srcConfig3, context);
+    suite3 = context.suite.exportConfig();
 
 vows.describe('compiler').addBatch({
     'suite 01': {
-        topic: compile(suite, path.join(__dirname, './assets/5/in/')),
+        topic: compile(suite1, assetPath),
         'should generate the correct suite name': function (suite) {
             assert.equal('test-suite-01.html', suite.name);
         },
-        // 'and the generated HTML': {
-        //     topic: function (suite) {
-        //         this.suite = suite;
-        //         fs.readFile('./tests/assets/5/out/test-suite-01.html', 'utf-8', this.callback)
-        //     },
-        //     'should match': function (html) {
-        //         var expected = md5(this.suite.html),
-        //             actual = md5(html);
+        'and the generated HTML': {
+            topic: function (suite) {
+                this.suite = suite;
+                fs.readFile('./tests/assets/5/out/test-suite-01.html', 'utf-8', this.callback)
+            },
+            'should match': function (html) {
+                var expected = md5(this.suite.html),
+                    actual = md5(html);
 
-        //         assert.equal(expected, actual);
-        //     }
-        // },
+                assert.equal(expected, actual);
+            }
+        },
         'and the assets': {
             topic: function (topic) {
                 return topic.assets;
@@ -52,18 +64,18 @@ vows.describe('compiler').addBatch({
             'should have a length of 1': function (assets) {
                 assert.equal(1, assets.length);
             },
-            // 'and the content': {
-            //     topic: function (topic) {
-            //         this.asset = topic[0];
-            //         fs.readFile('./tests/assets/5/out/assets/foo.html', 'utf-8', this.callback);
-            //     },
-            //     'should match': function (content) {
-            //         var expected = md5(this.asset.content),
-            //             actual = md5(content);
+            'and the content': {
+                topic: function (topic) {
+                    this.asset = topic[0];
+                    fs.readFile('./tests/assets/5/out/assets/foo.html', 'utf-8', this.callback);
+                },
+                'should match': function (content) {
+                    var expected = md5(this.asset.content),
+                        actual = md5(content);
 
-            //         assert.equal(expected, actual);
-            //     }
-            // },
+                    assert.equal(expected, actual);
+                }
+            },
             'and the mime should match': function (assets) {
                 assert.equal(assets[0].mime, 'text/html');
             },
@@ -72,43 +84,43 @@ vows.describe('compiler').addBatch({
             }
         }
     },
-    // 'suite 02': {
-    //     topic: compile(suites[1]),
-    //     'should generate the correct name': function (suite) {
-    //         assert.equal('test-suite-02.html', suite.name);
-    //     },
-    //     'and should assemble the assets': function (suite) {
-    //         assert.equal(0, suite.assets.length);
-    //     },
-    //     // 'and the generated HTML': {
-    //     //     topic: function (suite) {
-    //     //         this.suite = suite;
-    //     //         fs.readFile('./tests/assets/5/out/test-suite-02.html', 'utf-8', this.callback)
-    //     //     },
-    //     //     'should match': function (html) {
-    //     //         var expected = md5(this.suite.html),
-    //     //             actual = md5(html);
+    'suite 02': {
+        topic: compile(suite2, assetPath),
+        'should generate the correct name': function (suite) {
+            assert.equal('test-suite-02.html', suite.name);
+        },
+        'and should assemble the assets': function (suite) {
+            assert.equal(0, suite.assets.length);
+        },
+        'and the generated HTML': {
+            topic: function (suite) {
+                this.suite = suite;
+                fs.readFile('./tests/assets/5/out/test-suite-02.html', 'utf-8', this.callback)
+            },
+            'should match': function (html) {
+                var expected = md5(this.suite.html),
+                    actual = md5(html);
 
-    //     //         assert.equal(expected, actual);
-    //     //     }
-    //     // }
-    // },
-    // 'suite 03': {
-    //     topic: compile(suites[2]),
-    //     'should generate the correct name': function (suite) {
-    //         assert.equal('test-suite-03.html', suite.name);
-    //     },
-    //     // 'and the generated HTML': {
-    //     //     topic: function (suite) {
-    //     //         this.suite = suite;
-    //     //         fs.readFile('./tests/assets/5/out/test-suite-03.html', 'utf-8', this.callback)
-    //     //     },
-    //     //     'should match': function (html) {
-    //     //         var expected = (this.suite.html),
-    //     //             actual = (html);
+                assert.equal(expected, actual);
+            }
+        }
+    },
+    'suite 03': {
+        topic: compile(suite3, assetPath),
+        'should generate the correct name': function (suite) {
+            assert.equal('test-suite-03.html', suite.name);
+        },
+        'and the generated HTML': {
+            topic: function (suite) {
+                this.suite = suite;
+                fs.readFile('./tests/assets/5/out/test-suite-03.html', 'utf-8', this.callback)
+            },
+            'should match': function (html) {
+                var expected = (this.suite.html),
+                    actual = (html);
 
-    //     //         assert.equal(expected, actual);
-    //     //     }
-    //     // }
-    // }
+                assert.equal(expected, actual);
+            }
+        }
+    }
 }).export(module);
---
+++ tests/7-parser.js
@@ -0,0 +1,27 @@
+/**
+Copyright (c) 2013, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://yuilibrary.com/license/
+*/
+
+/*jslint es5: true*/ // complains about Vows' 'export' method
+"use strict";
+
+var vows = require('vows'),
+    assert = require('assert'),
+    path = require('path'),
+    fs = require('fs'),
+    parser = require('../lib/app/parser'),
+    raw = require(path.join(__dirname, 'assets/7/in/results.json'), 'utf-8'),
+    tests = {};
+
+tests['prettify'] = {
+    topic: parser.prettify(raw),
+   	'should exist': function (actual) {
+   		var expected = fs.readFileSync(path.join(__dirname, 'assets/7/out/table.txt'), 'utf-8');
+   		// console.log(actual, expected);
+		assert.equal(expected, actual);
+   	}
+};
+
+vows.describe('parser').addBatch(tests).export(module);
